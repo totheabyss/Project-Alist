8c5806c97ed5c72badf95fc9b1fa205a9bdc60b6
Augusto <tristedameianoite@gmail.com>
2026-01-17 18:06:44 -0300

    Change some features, fix the readme

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
- - PROJECT ALIST 
+ # PROJECT ALIST 
 
-- About
+## About
 It's a personal mini-blog that talk about music and reviews some albums using Javascript and PostgresSQL.
 
-- Technologies
-•Node.js
-•Express
-•EJS
-•CSS GRID
-•PostgresSQL
-•Music API
+##  Technologies
+• Node.js
 
-- Current Status
+• Express
+
+• EJS
+
+• CSS GRID
+
+• PostgresSQL
+
+• Music API
+
+##  Current Status
 Under development, frontend in advanced stage.
 
 -Features
@@ -19,10 +24,10 @@ Under development, frontend in advanced stage.
 •Custom header
 •Structured layout using css Grid
 
--Chalenges
+##  Chalenges
 •Aligning backend-oriented thinking with frontend layout logic
 
--Next Steps
+##  Next Steps
 •Implement backend with PostgresSQL
 •Integrate Music API
 •Add CRUD functionality
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import axios from "axios";
 import pg from "pg";
-import "dotenv/config";
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log("DB:", process.env.DB_NAME);
 
 const app = express();
 const port = 3000;
@@ -9,16 +12,42 @@ const db = new pg.Client({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT
 });
 db.connect();
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", (req,res) =>{
-    res.render("index.ejs");
-})
+async function getAlbums() {
+  const result = await db.query("SELECT * FROM albums");
+  return result.rows;
+}
+
+app.get("/", async (req,res) =>{
+    try{
+    const albums = await getAlbums();
+    res.render("index.ejs",{albums});
+    }catch(error){
+        console.log(error);
+        res.status(500).send("Internal Error");
+    }
+});
+
+app.get("/posts:id", async (req,res) =>{
+ try{
+    const albums = await getAlbums();
+    res.render("posts.ejs",{albums})
+}catch(error){
+        console.log(error);
+        res.status(500).send("Internal Error");
+    }
+});
+
+
+
+
+
 
 
 
